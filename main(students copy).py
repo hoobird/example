137e65735d9e9d1bb941364fe17b51677c68ce15
hoobirddemo <hoobirddemo@proton.me>
2024-01-08 19:45:26 +0800

    commit message

---
+++ main(students copy).py
@@ -0,0 +1,50 @@
+import turtle
+import time
+import random
+
+class Shape:
+    def __init__(self, x, y, shape, color):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def move(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def hide(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def show(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+
+class Text:
+
+    def __init__(self, x, y, text):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def write_text(self, text):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+
+class Game:
+
+    def __init__(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def create_shape(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def move_shapes(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def on_shape_click(self, x, y):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def update_timer(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+    def start(self):
+        pass  # REPLACE PASS WITH YOUR CODE
+
+
+if __name__ == "__main__":
+    pass  # REPLACE PASS WITH YOUR CODE
